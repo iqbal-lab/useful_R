5965f94c97179c5d428ced5f348d5979dacf5431
Zamin Iqbal <iqbal-lab@users.noreply.github.com>
2017-11-19 23:23:27 +0000

    First file

---
+++ snippets/data_loading/fast_load_large_tables/determine_col_classes.R
@@ -0,0 +1,6 @@
+
+# Load a few lines to determine types (class) of each column
+
+tab5rows <- read.table("datatable.txt", header = FALSE, nrows = 5)
+classes <- sapply(tab5rows, class)
+tabAll <- read.table("datatable.txt", header = FALSE, colClasses = classes)

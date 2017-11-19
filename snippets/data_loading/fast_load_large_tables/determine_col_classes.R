fa84058d632c737acaac0b2984d32bb65f03a984
Zamin Iqbal <iqbal-lab@users.noreply.github.com>
2017-11-19 23:24:10 +0000

    Add attribution

--- snippets/data_loading/fast_load_large_tables/determine_col_classes.R
+++ snippets/data_loading/fast_load_large_tables/determine_col_classes.R
@@ -4,3 +4,5 @@
 tab5rows <- read.table("datatable.txt", header = FALSE, nrows = 5)
 classes <- sapply(tab5rows, class)
 tabAll <- read.table("datatable.txt", header = FALSE, colClasses = classes)
+
+## from http://www.biostat.jhsph.edu/~rpeng/docs/R-large-tables.html
